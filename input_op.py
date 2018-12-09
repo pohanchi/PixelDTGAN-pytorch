4923c0298eaa434593746f2d0f2b949aa35bd962
pohan <aapp1420@gmail.com>
2018-12-09 11:27:25 +0800

    model_operation"
    /

---
+++ input_op.py
@@ -0,0 +1,215 @@
+import os
+import numpy as np
+import random
+from PIL import Image
+from os import listdir
+from os.path import isfile, isdir, join
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from tensorboardX import SummaryWriter
+import pickle
+from skimage import io, transform
+
+
+class mydataset(Dataset):
+  def __init__(self,filepath,opt,transform):
+    self.loadSize = opt.loadSize
+    self.fineSize = opt.fineSize
+    self.root = filepath
+    self.transform = transform
+    data=pickle.load(open(filepath,'rb'))
+    print('data_length=',len(data))
+    self.x = data[:58368]
+    random.shuffle(self.x)
+    self.X_train = np.asarray([i['data'] for i in self.x])
+    self.y_train = np.asarray([i['label'] for i in self.x])  
+    self.fake_train = np.asarray([i['not_label'] for i in self.x]) 
+    return
+  def __getitem__(self, index):
+    image_path='dataset/lookbook/data/'  #dataset file path
+
+    label = self.y_train[index]
+
+    label_name = image_path+label
+
+    image_name=image_path + self.X_train[index]
+
+    not_label_name = image_path + self.fake_train[index]
+
+    img = io.imread(image_name)
+    img_label = io.imread(label_name)
+    not_label = io.imread(not_label_name)
+    
+    associ_img = np.asarray(img_label)
+    in_image   = np.asarray(img)
+    noassoci_img= np.asarray(not_label)
+
+    data_type ={'input': in_image, 'associ': associ_img, 'noassoci': noassoci_img}
+    
+    if self.transform:
+        data_type=self.transform(data_type)
+    
+    return  data_type
+      
+  def __len__(self):
+    return len(self.x)
+
+class Rescale(object):
+
+  """Rescale the image in a sample to a given size.
+
+  Args:
+      output_size (tuple or int): Desired output size. If tuple, output is
+          matched to output_size. If int, smaller of image edges is matched
+          to output_size keeping aspect ratio the same.
+  """
+  def __init__(self, output_size):
+      assert isinstance(output_size, (int, tuple))
+      self.output_size = output_size
+
+  def __call__(self, sample):
+
+    image, label, no_label = sample['input'], sample['associ'],sample['noassoci']
+
+    h, w = image.shape[:2]
+    h_1, w_1 = label.shape[:2]
+    h_2, w_2 = no_label.shape[:2]
+
+    if isinstance(self.output_size, int):
+      if h > w:
+          new_h, new_w = self.output_size * h / w, self.output_size
+      else:
+          new_h, new_w = self.output_size, self.output_size * w / h
+      if h_1 > w_1:
+          new_h_1, new_w_1 = self.output_size * h_1 / w_1, self.output_size
+      else:
+          new_h_1, new_w_1 = self.output_size, self.output_size * w_1 / h_1
+      if h_2 > w_2:
+          new_h_2, new_w_2 = self.output_size * h_2 / w_2, self.output_size
+      else:
+          new_h_2, new_w_2 = self.output_size, self.output_size * w_2 / h_2
+    else:
+      new_h, new_w = self.output_size
+      new_h_1, new_w_1 = self.output_size
+      new_h_2, new_w_2 = self.output_size
+
+    new_h, new_w = int(new_h), int(new_w)
+    new_h_1,new_w_1 = int(new_h_1), int(new_w_1)
+    new_h_2,new_w_2 = int(new_h_2), int(new_w_2)
+
+
+    img = transform.resize(image, (new_h, new_w))
+    label = transform.resize(label, (new_h_1,new_w_1))
+    no_label=transform.resize(no_label,(new_h_2,new_w_2))
+
+    return {'input': img, 'associ': label, 'noassoci':no_label}
+
+class ToTensor(object):
+  """Convert ndarrays in samplcriterion = nn.BCELoss()e to Tensors."""
+
+  def __call__(self, sample):
+
+    image, label, no_label = sample['input'], sample['associ'], sample['noassoci']
+
+    # swap color axis because
+    # numpy image: H x W x C
+    # torch image: C X H X W
+    image = image.transpose((2, 0, 1))
+    label = label.transpose((2, 0, 1))
+    no_label = no_label.transpose((2, 0, 1))
+    return {'input': torch.from_numpy(image),
+            'associ': torch.from_numpy(label),
+            'noassoci':torch.from_numpy(no_label)}
+
+if __name__ == '__main__':
+  mypath = "dataset/lookbook/data/"
+
+  files = listdir(mypath)
+  print(len(files))
+
+  # for i,f in enumerate(files):
+  #   if i == 4:
+  #     temp=f[:9]
+  #     tmp=cv2.imread(mypath+f)
+  #     cv2.imshow('image',tmp)
+  #     k=cv2.waitKey(0)
+  #     if k == ord('s'):
+  #       cv2.destroyAllWindows()
+  #   if i >= 5 and f[:9]==temp and i<=77546:
+  #     print("file_name_",i,"=", f)
+  #     tmp=cv2.imread(mypath+f)
+  #     cv2.imshow('image',tmp)
+  #     k=cv2.waitKey(0)
+  #     if k == ord('s'):
+  #       cv2.destroyAllWindows()
+
+  category = list()
+  category_all =list()
+  for i,f in enumerate(files):
+    if i == 0:
+      temp =f[:9]
+      category += [temp]
+    if temp != f[:9]:
+      temp = f[:9]
+      category += [temp]
+  category=[i for i in set(category)]
+
+  data =list()
+  stocastic=list()
+  dict_={'data':None,'label':None,'not_label':None}
+  label_distribution={'data_num':None,'label':None}
+
+  for i in category:
+    for j,f in enumerate(files):
+      if f[:9] == i and f[10:16]=='CLEAN1':
+        category_all += [f]
+        del files[j]
+
+  for i in category_all:
+    k=0
+    tmp=[]
+    for j,f in enumerate(files):
+      if f[:9] == i[0:9]:
+        k=k+1
+        dict_['data']=f
+        dict_['label']=i
+        t = [ l for l in category_all if l != i]
+        no_associ=random.choice(t)
+        dict_['not_label']=no_associ
+        tmp+=[f]
+        data += [dict_.copy()]
+        del files[j]
+      else:
+        pass
+    label_distribution['data_num']= k
+    label_distribution['data']= tmp
+    label_distribution['label']=i
+    stocastic += [label_distribution.copy()]
+  print(data[:20])
+  print(stocastic[:20])
+  pickle.dump(data,open('data.p','wb'))
+  pickle.dump(stocastic,open('stocastic.p','wb'))
+  tmp=pickle.load(open('stocastic.p','rb'))
+  print(tmp[:3])
+
+
+
+
+
+  
+  
+      
+
+
+
---
+++ model.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import pickle
+
+class Generator(nn.Module):
+    def __init__(self,opt):
+        super(Generator, self).__init__()
+        self.ngpu = opt.ngpu
+        nc = 3
+        ngf = int(opt.ngf)
+        ndf = int(opt.ndf)
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x64
+            nn.Conv2d(nc, ngf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # state size. (ngf) x 32 x 32
+            nn.Conv2d(ngf, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # state size. (ngf*2) x 16 x 16
+            nn.Conv2d(ngf * 2, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            # state size. (ngf*2) x 8 x 8
+            nn.Conv2d(ngf * 4, ngf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf*8,ngf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf*4,ngf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            # state size. (ngf*4) x 16 x 16
+            nn.ConvTranspose2d(ngf*2,ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            # state size. (ngf*4) x 16 x 16
+            nn.ConvTranspose2d(ngf,nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input_):
+        if input_.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input_.float())
+        return output
+
+class Discriminator(nn.Module):
+    def __init__(self, opt):
+        super(Discriminator, self).__init__()
+        self.ngpu = opt.ngpu
+        nc = 3
+        ngf = int(opt.ngf)
+        ndf = int(opt.ndf)
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_):
+        if input_.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input_.float())
+
+        return output.view(-1, 1).squeeze(1)
+
+class Domain_Discriminator(nn.Module):
+
+    def __init__(self, opt):
+        super(Domain_Discriminator, self).__init__()
+        self.ngpu = opt.ngpu
+        nc = 3
+        ngf = int(opt.ngf)
+        ndf = int(opt.ndf)
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc*2, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+    def forward(self,input_):
+        if input_.is_cuda and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input_.float())
+
+        return output.view(-1, 1).squeeze(1)
+
+
